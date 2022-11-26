bc3527257d21e1320c50fde119cd37f2ca33c3fc
ape9 <santerikettu@outlook.com>
2022-11-26 20:13:40 +0200

    logging

--- common.h
+++ common.h
@@ -1,15 +1,16 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#include <iostream>
 #include <vector>
 #include <memory>
 #include <cassert>
 #include <string>
+#include <map>
+#include <fstream>
 #include "location.h"
 
 #ifdef DEBUG
-
-#include <iostream>
 #define DPRINT(x) std::cout << x << '\n';
 #endif
 
@@ -19,10 +20,88 @@ struct CompilerOptions {
 
 };
 
-}
+enum class logging_level {
+    DEBUG, INFO, WARNING, ERROR
+};
 
-namespace frontend {
+std::map<logging_level, std::string> LOGLEVELSTR = {
+    { logging_level::DEBUG, "DEBUG"}, 
+    { logging_level::INFO, "INFO" }, 
+    { logging_level::WARNING, "WARNING" }, 
+    { logging_level::ERROR, "ERROR" }
+};
 
+enum class logging_output {
+    STDOUT, STDERR, FILE
+};
+
+struct log {
+    log() = default;
+    log(const Location& loc, 
+        const std::string& msg, 
+        logging_level lvl)
+        : location(loc), 
+        message(msg),
+        level(lvl) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const log& l) {
+        os << "[" << LOGLEVELSTR[l.level] << "]" 
+           << " in file " << l.location._fileName
+           << " row " << l.location._row << " col " << l.location._col 
+           << ": " << l.message;
+        return os;
+    }
+
+    Location location;
+    std::string message;
+    logging_level level;
+};
+
+struct logger {
+    static void debug(const Location& loc, const std::string& msg) {
+        if (level != logging_level::DEBUG) {
+            return;
+        }
+        logs.push_back(log(loc, msg, logging_level::DEBUG));
+    }
+
+    static void info(const Location& loc, const std::string& msg) {
+        if (level == logging_level::DEBUG) {
+            return;
+        }
+        logs.push_back(log(loc, msg, logging_level::INFO));
+    }
+
+    static void warn(const Location& loc, const std::string& msg) {
+        if (level == logging_level::INFO || level == logging_level::DEBUG) {
+            return;
+        }
+        logs.push_back(log(loc, msg, logging_level::WARNING));
+        warning_count++;
+    }
+    
+    static void error(const Location& loc, const std::string& msg) {
+        if (level == logging_level::ERROR) {
+            logs.push_back(log(loc, msg, logging_level::ERROR));
+            error_count++;
+        }
+    }
+
+    static void do_log() {
+        for (auto& msg : logs) {
+            std::cout << msg << '\n';
+        }
+        std::cout << error_count << " error(s).\n";
+        std::cout << warning_count << " warning(s).\n";
+    }
+
+    static inline logging_level level{ logging_level::ERROR };
+    static inline logging_output output_dest{ logging_output::STDOUT };
+    static inline std::string outfile{ "logs.txt" };
+    static inline std::vector<log> logs{};
+    static inline int error_count{ 0 };
+    static inline int warning_count{ 0 };
+};
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-#include "lexer.h"
-
+#include "common.h"
 
 int main(int argc, char **argv) {
-
-    auto lexer = frontend::Lexer("test.txt");
-    auto tokens = lexer.createTokens();
-    for (auto& tok : tokens) {
-        tok->dump();
-    }
-
+    utils::Location loc("test.c", 95, 22);
+    utils::logger::level = utils::logging_level::ERROR;
+    utils::logger::error(loc, "WTF IS THIS!?!?");
+    utils::logger::do_log();
     return 0;
 }
